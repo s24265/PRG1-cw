f1f5957e66da2a93bab93b37a65b551d887ad308
s24265 <91980319+s24265@users.noreply.github.com>
2021-11-12 10:16:38 +0100

    Add files via upload

---
+++ 2/2.0.0.1.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+
+int main(){
+	int a;
+	std::cout << "Podaj pierwszą liczbę: ";
+	std::cin >> a;
+	int b;
+	std::cout << "Podaj drugą liczbę: ";
+	std::cin >> b;
+	
+	if (b > a){
+		for (int i = a; i < b; i++){
+		std::cout << i << "\n";
+		}
+	}
+	return 0;
+}
