3b21102ab8ca74f07b642928befef2d8560ebd9e
s24265 <91980319+s24265@users.noreply.github.com>
2021-11-19 20:21:56 +0100

    Add files via upload

---
+++ 3/3.1.0.1.cpp
@@ -0,0 +1,21 @@
+#include <iostream>
+
+auto init(int a[], int n) -> void;
+
+auto main(int argc, char* argv[]) -> int{
+	int a[1234];
+	int n = std::stoi(argv[1]);
+	init(a, n);
+
+	for (int i = 0; i < n; i++){
+		std::cout << a[i];
+		//std::cout << "\n";
+	}
+	std::cout << "\n";
+	return 0;
+}
+void init(int a[], int n){
+	for (int i = 0; i < n; i++){
+		a[i] = 0;
+	}
+}
---
+++ 3/3.1.0.2.cpp
@@ -0,0 +1,21 @@
+#include <iostream>
+
+auto iota(int a[], int n, int start) -> void;
+
+auto main(int argc, char* argv[]) -> int{
+	int a[1234];
+	int n = std::stoi(argv[1]);
+	int start = std::stoi(argv[2]);
+	iota (a, n, start);
+
+	for (int i = 0; i < n; i++){
+		std::cout << a[i];
+	}
+	std::cout << "\n";
+	return 0;
+}
+void iota(int a[], int n, int start){
+	for (int i = 0; i < n; i++){
+		a[i] = start + i;
+	}
+}
