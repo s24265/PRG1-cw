f138d76e82a20b25138e993eb5009e555c38f6d9
s24265 <91980319+s24265@users.noreply.github.com>
2021-11-25 23:44:54 +0100

    Add files via upload

---
+++ 3/3.1.0.4.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+#include <array>
+
+auto amin(int a[], int n) -> int{
+    int najwartosc = 0;
+
+    for (int i = 0; i < n; i++){
+        if (a[i] < najwartosc){
+            najwartosc = a[i];
+        }
+    }
+    return najwartosc;
+}
+auto main() -> int{
+    std::array<int, 10> przyklad { 42, 9, -1, 18, 59, 3, 101, 31, 72, 12 };
+    std::cout << "Najmniejsza wartość to: " << amin(przyklad.data(), przyklad.size()) << "\n";
+    return 0;
+}
