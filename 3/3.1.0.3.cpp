21e9449df1723051edae05bfbc766309be517d66
s24265 <91980319+s24265@users.noreply.github.com>
2021-11-25 22:52:09 +0100

    Update 3.1.0.3.cpp

--- 3/3.1.0.3.cpp
+++ 3/3.1.0.3.cpp
@@ -1,17 +1,16 @@
 #include <iostream>
-#include <array> //tab
+#include <array>
 
-auto asum(int a[], int n) -> int;
-
-auto main() -> int{
-	std::array<int, 10> przyklad { 42, 9, -1, 18, 59, 3, 101, 31, 72, 12 };
-	std::cout << "Suma to: " << asum(przyklad.data(), przyklad.size()) << "\n";
-	return 0;
-}
-int asum(int a[], int n){
+auto asum(int a[], int n) -> int{
 	int wynik = 0;
+
 	for (int i = 0; i < n; i++){
 		wynik += a[i];
 	}
 	return wynik;
+}
+auto main() -> int{
+	std::array<int, 10> przyklad { 42, 9, -1, 18, 59, 3, 101, 31, 72, 12 };
+	std::cout << "Suma to: " << asum(przyklad.data(), przyklad.size()) << "\n";
+	return 0;
 }
