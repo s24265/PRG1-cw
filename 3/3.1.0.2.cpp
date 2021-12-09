3567a514d69e2c630f4bcdf4e6ee5be8573a4840
s24265 <91980319+s24265@users.noreply.github.com>
2021-12-09 23:36:35 +0100

    Update 3.1.0.2.cpp

--- 3/3.1.0.2.cpp
+++ 3/3.1.0.2.cpp
@@ -1,19 +1,21 @@
 #include <iostream>
 
-auto main(int argc, char* argv[]) -> int{
+auto iota(int a[], int n, int start) -> void{
+	for (int i = 0; i < n; i++){
+		a[i] = start + i;
+	}
+}
+auto main() -> int{
+	int n;
+	int start;
+	std::cin >> n;
+	std::cin >> start;
 	int a[1234];
-	int n = std::stoi(argv[1]);
-	int start = std::stoi(argv[2]);
 	iota (a, n, start);
-
+	
 	for (int i = 0; i < n; i++){
 		std::cout << a[i];
 	}
 	std::cout << "\n";
 	return 0;
-}
-void iota(int a[], int n, int start){
-	for (int i = 0; i < n; i++){
-		a[i] = start + i;
-	}
 }
