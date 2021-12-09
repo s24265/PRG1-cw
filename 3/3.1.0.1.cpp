82ef75f9724cfecec0ba01d61693df168196101f
s24265 <91980319+s24265@users.noreply.github.com>
2021-12-09 23:27:34 +0100

    Update 3.1.0.1.cpp

--- 3/3.1.0.1.cpp
+++ 3/3.1.0.1.cpp
@@ -1,19 +1,20 @@
 #include <iostream>
 
-auto main(int argc, char* argv[]) -> int{
+auto init(int a[], int n) -> void{
+	for (int i = 0; i < n; i++){
+		a[i] = 0;
+	}
+}
+auto main() -> int{
+	int n;
+	std::cout << "Podaj rozmiar tab: ";
+	std::cin >> n;
 	int a[1234];
-	int n = std::stoi(argv[1]);
 	init(a, n);
 
 	for (int i = 0; i < n; i++){
 		std::cout << a[i];
-		//std::cout << "\n";
 	}
 	std::cout << "\n";
 	return 0;
-}
-void init(int a[], int n){
-	for (int i = 0; i < n; i++){
-		a[i] = 0;
-	}
 }
