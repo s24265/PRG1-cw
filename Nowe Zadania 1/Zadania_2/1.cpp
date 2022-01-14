966db3a3cb6e6ffa4b23a75c3b5e25912b3006d6
s24265 <91980319+s24265@users.noreply.github.com>
2022-01-14 01:00:34 +0100

    Add files via upload

---
+++ Nowe Zadania 1/Zadania_2/1.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+#include <vector>
+#include <cmath>
+
+auto average_of(std::vector<double> wektor) -> float{
+	float suma = 0;
+
+	for (int i = 0; i < wektor.size(); i++){
+		suma += wektor[i];
+	}
+	float sredniaa = suma / wektor.size(); //s arytmetyczna
+	return sredniaa;
+}
+auto main() -> int{
+	auto lrzeczywiste = std::vector<double>{1, 2, 3, 4, 5, }
+}
